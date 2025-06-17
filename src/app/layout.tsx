c37220926b9dfccf2a16f1e74a676d8e096432df
M4thez <namemat@gmail.com>
2025-06-17 16:15:01 +0200

    GitHub Pages fix #2

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -9,8 +9,9 @@ config.autoAddCss = false;
 const roboto = Roboto({ weight: ["300", "400", "700"], subsets: ["latin"] });
 
 export const metadata: Metadata = {
-  title: "Opera Frontend Intern Task",
-  description: "React frontend intern task for Opera by Mateusz Boboryko.",
+  title: "Video Games Media Slider ",
+  description:
+    "A video games media slider with custom music made by Mateusz Boboryko.",
 };
 
 export default function RootLayout({
